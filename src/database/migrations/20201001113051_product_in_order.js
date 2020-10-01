83ee2570948d210c2634a463811ec6d9aeaea17f
Paloma <palomaazevedocosta@gmail.com>
2020-10-01 19:17:12 -0300

    Alterações na migration product_in_order

--- src/database/migrations/20201001113051_product_in_order.js
+++ src/database/migrations/20201001113051_product_in_order.js
@@ -2,7 +2,8 @@
 exports.up = function(knex) {
     return knex.schema.createTable('product_in_order', function(table){
     table.integer('product_in_order_id').primary();
-    table.foreign('product_in_order_id').references('shirt_model_id').inTable('shirt_model');
+    table.integer('shirt_model').notNullable();
+    table.foreign('shirt_model').references('shirt_model_id').inTable('shirt_model');
     table.string('order_id').primary();
     table.foreign('order_id').references('order_id').inTable('order');
     table.string('product_id').notNullable();
