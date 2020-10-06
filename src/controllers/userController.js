1b39465c91daed42a63e19c24b1abfe093f75f48
Paloma <palomaazevedocosta@gmail.com>
2020-10-06 19:02:25 -0300

    Criação dos models e inicio do controllers

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,7 +1,43 @@
-//cria o ponto create
+const UsersModel = require('../models/UsersModel');
+const AdressModel = require('../models/AdressModel');
 
 module.exports={
-    async create(request,response){
-
+    async createUser(request,response){
+        const user = {
+            name: request.body.name,
+            user_type: request.body.user_type,
+            email: request.body.email,
+            cpf: request.body.cpf,
+        };
+        const adress = request.body.adress,
+        try {
+            await usersModel.create(user);
+            await adress.forEach(adress => {
+                user_id = user.createdUser;
+            });
+            await AdressModel.create(adress);
+            response.status(200).json('Endereço criado com sucesso');
+        } catch (error) {
+            console.log(error.message);
+            response.status(500).json('internal server error');
+        }
     },
+    async allClients(request,response){
+    
+    },
+    async getAdresses(request,response){
+    
+    },
+    async allAdm(request,response){
+    
+    },
+    async deleteUser(request,response){
+    
+    },
+    async updateAdress(request,response){
+    
+    },
+    async updateUser(request,response){
+    
+    }
 }
--- src/models/AdressModel.js
+++ src/models/AdressModel.js
@@ -2,9 +2,9 @@ const { create } = require('../controllers/userController');
 const connection = require ('../database/connection');
 
 module.exports = {
-    async create(user){
+    async create(adress){
         try {
-            const response = await connection ('user').insert(user);
+            const response = await connection ('adress').insert(adress);
             return response;
         } catch (error) {
             console.log(error.message);
@@ -13,7 +13,7 @@ module.exports = {
     },
     async read(){
         try {
-            const response = await connection ('user').select('*');
+            const response = await connection ('adress').select('*');
             return response;
         } catch (error) {
             console.log(error.message);
@@ -22,16 +22,25 @@ module.exports = {
     },
     async getById(id){
         try {
-            const response = await connection ('user').where('user_id', id).select('*');
+            const response = await connection ('adress').where('adress_id', id).select('*');
             return response;
         } catch (error) {
             console.log(error.message);
             return error;
         }
     },
-    async update(user_id, updated_user){
+    async getAdressByUserId(user_id){
         try {
-            const response = await connection ('user').where('user_id', user_id).update(updated_user);
+            const response = await connection ('adress').where('user_id', user_id).select('*');
+            return response;
+        } catch (error) {
+            console.log(error.message);
+            return error;
+        }
+    },
+    async update(adress_id, updated_adress){
+        try {
+            const response = await connection ('adress').where('adress_id', adress_id).update(updated_adress);
             return response;
         } catch (error) {
             console.log(error.message);
@@ -40,7 +49,7 @@ module.exports = {
     },
     async delete(id){
         try {
-            const response = await connection ('user').where('user_id', user_id).del();
+            const response = await connection ('adress').where('adress_id', adress_id).del();
             return response;
         } catch (error) {
             console.log(error.message);
