8d53003b04fb73b6804e37757f9412ff36b05f4d
Gabriel Andrade <gabrielolandrade@hotmail.com.br>
2020-10-08 12:16:34 -0300

    Terminamos e testamos todos os metodos
    Falta apenas pegarmos os parametros cm o req.params

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,88 +1,120 @@
-const UsersModel = require('../models/UsersModel');
-const AdressModel = require('../models/AdressModel');
+const UsersModel = require("../models/UsersModel");
+const AdressModel = require("../models/AdressModel");
 
-module.exports={
-    async createUser(request,response){
-        const user = {
-            name: request.body.name,
-            user_type: request.body.user_type,
-            email: request.body.email,
-            cpf: request.body.cpf,
-        };
-        const adress = { 
-            adress: request.body.adress,
-        };
+const uuid = require("react-uuid");
 
+//estamos recebendo alguns erros que não estão estourando na tela, apesar de que o objeto
+//nao vem senod cirado
+module.exports = {
+    async createUser(request, response) { //ok
         try {
+            const user = {
+                name: request.body.name,
+                user_type: request.body.user_type,
+                email: request.body.email,
+                cpf: request.body.cpf,
+            };
+
+            user.user_id = uuid();
+
+            const adress = {
+                street: request.body.address.street,
+                neighborhood: request.body.address.neighborhood,
+                city: request.body.address.city,
+                state: request.body.address.state,
+                zip_code: request.body.address.zip_code,
+                country: request.body.address.country,
+                complement: request.body.address.complement,
+            };
+
             await UsersModel.create(user);
-            /*await adress.forEach(adress => {
-                user_id = user.createdUser;
-            });*/
+
+            adress.user_id = user.user_id;
+
             await AdressModel.create(adress);
-            response.status(200).json('Endereço criado com sucesso');
+
+            response.status(200).json("Usuário criado com sucesso");
         } catch (error) {
             console.log(error.message);
-            response.status(500).json('internal server error');
+            response.status(500).json("internal server error");
         }
     },
-    async allClients(request,response){
+    async allClients(request, response) {//ok
         try {
-            const clients = await UsersModel.getAllByTypes('client');
-            response.status(200).json({clients});
+            const clients = await UsersModel.getAllByTypes("client");
+            response.status(200).json({ clients });
         } catch (error) {
             console.log(error.message);
-            response.status(500).json('internal server error');
+            response.status(500).json("internal server error");
         }
     },
-    async getAdresses(request,response){
+    async getAdresses(request, response) {//ok //vamos mudar pra req.params
         const user_id = request.body.user_id;
         try {
-            const response = await AdressModel.getAdressByUserId(user_id);
-            return response;
+            const adresses = await AdressModel.getAdressByUserId(user_id);
+            response.status(200).json({ adresses });
         } catch (error) {
             console.log(error.message);
-            response.status(500).json('internal server error');
+            response.status(500).json("internal server error");
         }
     },
-    async allAdm(request,response){
+    async allAdm(request, response) { //ok
         try {
-            const response = await UsersModel.getAllByTypes('adm');
-            return response;
+            const adms = await UsersModel.getAllByTypes("adm");
+            response.status(200).json({ adms });
         } catch (error) {
             console.log(error.message);
-            response.status(500).json('internal server error');
-        }    
+            response.status(500).json("internal server error");
+        }
     },
-    async deleteUser(request,response){
+    async deleteUser(request, response) {//ok // mudar pro req
+
         const id = request.body.user_id;
+
         try {
-            const response = await UsersModel.delete(id);
-            return response.status(200).json('Apagado com sucesso');
+            await UsersModel.delete(id);
+            response.status(200).json("Apagado com sucesso");
         } catch (error) {
             console.log(error.message);
-            response.status(500).json('internal server error');
+            response.status(500).json("internal server error");
         }
     },
-    async updateAdress(request,response){
-        const {adress_id} = request.params;
-        const {updated_adress} = request.body;
+    async updateAddress(request, response){//ok //mudar pro req
+        /* const { } = request.params; */
+        const {address_id, updated_address } = request.body;
         try {
-            const response = await AdressModel.update(adress_id, updated_adress);
-            response.status(200).json('alterado com sucesso');
+            await AdressModel.update(
+                address_id,
+                updated_address
+            );
+            console.log(updated_address)
+            response.status(200).json("alterado com sucesso");
         } catch (error) {
             console.log(error.message);
-            response.status(500).json('internal server error');
+            response.status(500).json("internal server error");
         }
     },
-    async updateUser(request,response){
-        const {user_id} = request.params;
-        const {updated_user} = request.body;
+    async updateUser(request, response) { //ok //mudar pro req
+        /* const {  } = request.params; */
+        const { user_id , updated_user } = request.body;
         try {
-            const response = await UsersModel.update(user_id, updated_user);
-            response.status(200).json('alterado com sucesso');
+            await UsersModel.update(user_id, updated_user);
+            response.status(200).json("alterado com sucesso");
         } catch (error) {
+            console.log(error.message);
+            response.status(500).json("internal server error");
+        }
+    },
+    async deleteAddress(request,response){ //ok //mudar pro req
+        try{
+            const id = request.body.address_id;
+
+            await AdressModel.delete(id);
+
+            response.status(200).json('Apagado com sucesso')
+        }catch(error){
             console.log(error.message);
             response.status(500).json('internal server error');
         }
     }
-}
+};
