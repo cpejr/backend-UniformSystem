fd6a2fde656edd1b19c6c9b6fe3ee675e54c4e86
Bryan Azevedo <bryan42damasceno@gmail.com>
2021-02-10 23:50:17 -0300

    ADD rotas do Bryan

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,13 +1,17 @@
-const express = require('express');
+const express = require("express");
 
-const usersRouter = require('./users');
-const sessionRouter = require('./session');
+const usersRouter = require("./users");
+const sessionRouter = require("./session");
+const addressRouter = require("./address");
+const productModelRouter = require("./productModel");
 
 const routes = express.Router();
 
-routes.use('/users', usersRouter);
+routes.use("/users", usersRouter);
 
-routes.use('/session', sessionRouter);
+routes.use("/session", sessionRouter);
 
+routes.use("/address", addressRouter);
+routes.use("/productmodels", productModelRouter);
 
 module.exports = routes;
---
+++ src/routes/productModel/index.js
@@ -0,0 +1,53 @@
+const express = require("express");
+const productModelRouter = express.Router();
+const { celebrate } = require("celebrate");
+
+const productController = require("../../controllers/productController");
+const productModelController = require("../../controllers/productModelController");
+const productValidator = require("../../validators/productValidator");
+
+const upload = require("../../utils/multer");
+
+const {
+  authenticateToken,
+  isAdmin,
+} = require("../../middlewares/authentication");
+
+productModelRouter.post(
+  "/newmodel/:product_id",
+  authenticateToken,
+  isAdmin,
+  upload,
+  celebrate(productValidator.addProductModel),
+  productModelController.addProductModel
+);
+
+productModelRouter.get(
+  "/:product_id",
+  celebrate(productValidator.getProductModel),
+  productModelController.getProductModel
+);
+productModelRouter.get(
+  "/",
+  celebrate(productValidator.allModels),
+  productController.allModels
+);
+
+productModelRouter.delete(
+  "/model/:model_id",
+  celebrate(productValidator.deleteModel),
+  authenticateToken,
+  isAdmin,
+  productModelController.deleteModel
+);
+
+productModelRouter.put(
+  "/model/:model_id",
+  authenticateToken,
+  isAdmin,
+  upload,
+  celebrate(productValidator.updateModel),
+  productModelController.updateModel
+);
+
+module.exports = productModelRouter;
