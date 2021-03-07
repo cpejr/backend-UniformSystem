f4fb76175e1e59dc7b19dd96d0d69e10278b3b70
Diogo Almazan <diogodpa@outlook.com>
2021-03-07 20:01:43 -0300

    Ajustes knexfile sem db_port em staging

--- knexfile.js
+++ knexfile.js
@@ -21,7 +21,6 @@ module.exports = {
     client: "pg",
     connection: {
       host: process.env.DB_HOST,
-      port: process.env.DB_PORT,
       database: process.env.DB_NAME,
       user: process.env.DB_USER,
       password: process.env.DB_PASS,
