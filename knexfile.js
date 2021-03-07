1114e61a8c4425465b6c783f8c03c6c4e7e3acbe
Diogo Almazan <diogodpa@outlook.com>
2021-03-07 20:00:36 -0300

    Ajustes knexfile configurações do banco em staging

--- knexfile.js
+++ knexfile.js
@@ -18,11 +18,13 @@ module.exports = {
   },
 
   staging: {
-    client: "postgresql",
+    client: "pg",
     connection: {
-      database: "my_db",
-      user: "username",
-      password: "password",
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT,
+      database: process.env.DB_NAME,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASS,
     },
     pool: {
       min: 2,
