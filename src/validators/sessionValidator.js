dc5cfd97585a9b170fcf1ca001aed9770d53d742
Diogo Almazan <diogodpa@outlook.com>
2021-03-16 12:20:32 -0300

    Validator de forgotPassword

--- src/validators/sessionValidator.js
+++ src/validators/sessionValidator.js
@@ -4,14 +4,14 @@ const sessionValidator = new Object();
 
 sessionValidator.signIn = {
     [Segments.BODY]: Joi.object().keys({
-        email: Joi.string().required(),
+        email: Joi.string().email().required(),
         password: Joi.string().required(),
     }),
 };
 
 sessionValidator.forgetPassword = {
     [Segments.BODY]: Joi.object().keys({
-        email: Joi.string().required(),
+        email: Joi.string().email().required(),
     }),
 };
 
